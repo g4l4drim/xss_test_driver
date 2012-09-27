502d677548cc8f06d23f917a02fe6c3083e9f347
g4l4drim <pingouin@gentoo.labosecu.rennes.telecom-bretagne.eu>
2012-09-27 23:07:01 +0200

    woops, need coffee

---
+++ urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls.defaults import *
+
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'xssdb.views.home', name='home'),
+    # url(r'^xssdb/', include('xssdb.foo.urls')),
+    url(r'^admin/shazzerimport/$', 'xssdb.xdb.admin_views.shazzer_import'),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #website index :
+    url(r'^', include('xssdb.xdb.urls')),
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
