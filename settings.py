f69dfff1bbd6a10ceace6d72c0ebb4f1e265bd9e
Galadrim <g4l4drim@gmail.com>
2012-09-28 17:38:25 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -139,7 +139,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    "xssdb.xdb"
+    "xdb"
 )
 
 # A sample logging configuration. The only tangible logging
